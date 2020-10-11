0949ad4e640d4f154e17993b6b274ba4630dc939
Odeke12 <trevodex@gmail.com>
2020-10-11 12:33:17 +0300

    E-commerce early stages
    Adds to cart and removes from cart

---
+++ ecommerce/store/static/js/cart.js
@@ -0,0 +1,40 @@
+var updateBtns = document.getElementsByClassName("update-cart")
+
+for(i = 0; i < updateBtns.length; i ++){
+    updateBtns[i].addEventListener('click', function(){
+        var productid = this.dataset.product
+        var action = this.dataset.action
+        console.log('productid:',productid, 'action:', action)
+
+        console.log(`User : ${user}`)
+
+        if (user == "AnonymousUser"){
+            console.log("User is not logged in")
+        }else{
+            updateUserOrder(productid, action)
+        }
+        
+    })
+}
+
+
+function updateUserOrder(productid, action){
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({'productId': productid, 'action' : action})
+    })
+    .then((response) => {
+        return response.json()
+
+    })
+    .then((data) => {
+        console.log('data:', data)
+    })
+}
